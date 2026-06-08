661336c9fdd997471218fb447c0f957cada7e0a1
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-06-08 15:21:55 +0000

    aaaaaaaaaaaaaaa

--- procurando-aluno.js
+++ procurando-aluno.js
@@ -6,6 +6,12 @@ const listaDeAlunosEMedias = [alunos, medias];
 function exibieNomeENota(aluno) {
     if (listaDeAlunosEMedias[0].includes(aluno)) {
         console.log(`${aluno} está cadastrado`);
+
+const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+        const media = listaDeAlunosEMedias[1][indice];
+        console.log(`A média de ${aluno} é ${media}`);
+
+
     } else {
         console.log("O aluno não foi encontrado!");
     }
